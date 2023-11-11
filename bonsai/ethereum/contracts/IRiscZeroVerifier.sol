2d2d55daad5e61b88d75fe33c60e59b4a78d2541
Daniel Helm <dan.helm@gmail.com>
2023-11-10 18:20:43 -0600

    use hashed constant incase not happening at compile

--- bonsai/ethereum/contracts/IRiscZeroVerifier.sol
+++ bonsai/ethereum/contracts/IRiscZeroVerifier.sol
@@ -20,82 +20,90 @@ import {ISHA256, SHA256_ADDR} from "./ISHA256.sol";
 
 /// @notice Indicator for the overall system at the end of execution covered by this proof.
 enum SystemExitCode {
-    Halted,
-    Paused,
-    SystemSplit
+  Halted,
+  Paused,
+  SystemSplit
 }
 
 /// @notice Combination of system and user exit codes.
 /// @dev If system exit code is SystemSplit, the user exit code must be zero.
 struct ExitCode {
-    SystemExitCode system;
-    uint8 user;
+  SystemExitCode system;
+  uint8 user;
 }
 
 /// @notice Data associated with a receipt which is used for both input and
 /// output of global state.
 struct ReceiptMetadata {
-    /// Digest of the SystemState of a segment just before execution has begun.
-    bytes32 preStateDigest;
-    /// Digest of the SystemState of a segment just after execution has completed.
-    bytes32 postStateDigest;
-    /// The exit code for a segment
-    ExitCode exitCode;
-    /// A digest of the input, from the viewpoint of the guest.
-    bytes32 input;
-    /// A digest of the journal, from the viewpoint of the guest.
-    bytes32 output;
+  /// Digest of the SystemState of a segment just before execution has begun.
+  bytes32 preStateDigest;
+  /// Digest of the SystemState of a segment just after execution has completed.
+  bytes32 postStateDigest;
+  /// The exit code for a segment
+  ExitCode exitCode;
+  /// A digest of the input, from the viewpoint of the guest.
+  bytes32 input;
+  /// A digest of the journal, from the viewpoint of the guest.
+  bytes32 output;
 }
 
 library ReceiptMetadataLib {
-    ISHA256 constant sha256Hasher = ISHA256(SHA256_ADDR);
-    // Compiler sha256: Initial value for constant variable has to be compile-time constant.
-    bytes32 constant TAG_DIGEST = sha256("risc0.ReceiptMeta");
+  ISHA256 constant sha256Hasher = ISHA256(SHA256_ADDR);
+  // Compiler sha256: Initial value for constant variable has to be compile-time constant.
+  bytes32 constant TAG_DIGEST =
+    0xef686daf1a76d75a617c189942563937860c85efc86a3e2c8304b47223ae3c7d;
 
-    function digest(ReceiptMetadata memory meta) internal pure returns (bytes32) {
-        return sha256Hasher.hash(
-            abi.encodePacked(
-                TAG_DIGEST,
-                // down
-                meta.input,
-                meta.preStateDigest,
-                meta.postStateDigest,
-                meta.output,
-                // data
-                uint32(meta.exitCode.system) << 24,
-                uint32(meta.exitCode.user) << 24,
-                // down.length
-                uint16(4) << 8
-            )
-        );
-    }
+  // bytes32 constant TAG_DIGEST = sha256("risc0.ReceiptMeta");
+
+  function digest(ReceiptMetadata memory meta) internal pure returns (bytes32) {
+    return
+      sha256Hasher.hash(
+        abi.encodePacked(
+          TAG_DIGEST,
+          // down
+          meta.input,
+          meta.preStateDigest,
+          meta.postStateDigest,
+          meta.output,
+          // data
+          uint32(meta.exitCode.system) << 24,
+          uint32(meta.exitCode.user) << 24,
+          // down.length
+          uint16(4) << 8
+        )
+      );
+  }
 }
 
 struct Receipt {
-    bytes seal;
-    ReceiptMetadata meta;
+  bytes seal;
+  ReceiptMetadata meta;
 }
 
 interface IRiscZeroVerifier {
-    /// @notice verify that the given receipt is a valid Groth16 RISC Zero recursion receipt.
-    /// @return true if the receipt passes the verification checks.
-    function verify(Receipt calldata receipt) external view returns (bool);
+  /// @notice verify that the given receipt is a valid Groth16 RISC Zero recursion receipt.
+  /// @return true if the receipt passes the verification checks.
+  function verify(Receipt calldata receipt) external view returns (bool);
 
-    /// @notice verifies that the given seal is a valid Groth16 RISC Zero proof of execution over the
-    ///     given image ID, post-state digest, and journal. Asserts that the input hash
-    //      is all-zeros (i.e. no committed input) and the exit code is (Halted, 0).
-    /// @return true if the receipt passes the verification checks.
-    function verify(bytes calldata seal, bytes32 imageId, bytes32 postStateDigest, bytes32 journalHash)
-        external
-        view
-        returns (bool);
+  /// @notice verifies that the given seal is a valid Groth16 RISC Zero proof of execution over the
+  ///     given image ID, post-state digest, and journal. Asserts that the input hash
+  //      is all-zeros (i.e. no committed input) and the exit code is (Halted, 0).
+  /// @return true if the receipt passes the verification checks.
+  function verify(
+    bytes calldata seal,
+    bytes32 imageId,
+    bytes32 postStateDigest,
+    bytes32 journalHash
+  ) external view returns (bool);
 
-    /// @notice verifies that the given seal is a valid Groth16 RISC Zero proof of execution over the
-    ///     given image ID, post-state digest, and full journal. Asserts that the input hash
-    //      is all-zeros (i.e. no committed input) and the exit code is (Halted, 0).
-    /// @return true if the receipt passes the verification checks.
-    function verify(bytes memory seal, bytes32 imageId, bytes32 postStateDigest, bytes calldata journal)
-        external
-        view
-        returns (bool);
+  /// @notice verifies that the given seal is a valid Groth16 RISC Zero proof of execution over the
+  ///     given image ID, post-state digest, and full journal. Asserts that the input hash
+  //      is all-zeros (i.e. no committed input) and the exit code is (Halted, 0).
+  /// @return true if the receipt passes the verification checks.
+  function verify(
+    bytes memory seal,
+    bytes32 imageId,
+    bytes32 postStateDigest,
+    bytes calldata journal
+  ) external view returns (bool);
 }
